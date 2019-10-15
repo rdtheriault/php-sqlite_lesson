b72eb6d584716324562af4b0fc97cc8298d7d3e2
Robert <rdtheriault@yahoo.com>
2019-10-15 10:22:04 -0700

    Create basic.php

---
+++ php/basic.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <style>
+      #peeps {
+        color:red;
+      }
+    </style>
+  </head>
+  <body>
+    <?php 
+      echo "Hello World";
+      echo "<h1 id='peeps'>Hello People</h1>";
+    ?>
+  </body>
+</html>
