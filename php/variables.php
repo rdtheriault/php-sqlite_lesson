a9ab50055e256e53ad22b06d8ac3e47ac5747a2f
Robert <rdtheriault@yahoo.com>
2019-11-12 15:46:52 -0800

    Create variables.php

---
+++ php/variables.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <style>
+      #peeps {
+        color:red;
+      }
+    </style>
+  </head>
+  <body>
+    <?php 
+	    $myFirstVar = "Hello World";
+      echo $myFirstVar;
+      echo "<h1 id='peeps'>".$myFirstVar."</h1>";
+    ?>
+  </body>
+</html>
