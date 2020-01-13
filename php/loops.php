04cf65f1fb9adab7639d7a9051e10c1c520d0fe1
Robert <rdtheriault@yahoo.com>
2020-01-13 11:06:39 -0800

    Add files via upload

---
+++ php/loops.php
@@ -0,0 +1,24 @@
+<html>
+  <head>
+    <style>
+      #peeps {
+        color:red;
+      }
+    </style>
+  </head>
+  <body>
+    <?php 
+	  for ($i = 0; $i < 5; $i++){
+		  echo $i."<br>";
+	  }
+	  
+	  $x = 0;
+	  while($x < 5){
+		  echo $x."x<br>";
+		  $x++;
+		  
+	  }
+	  
+    ?>
+  </body>
+</html>
