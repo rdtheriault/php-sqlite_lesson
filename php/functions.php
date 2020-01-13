e26ea760791811f6513fa1f351cd210b7d3e66d0
Robert <rdtheriault@yahoo.com>
2020-01-13 11:07:58 -0800

    Add files via upload

---
+++ php/functions.php
@@ -0,0 +1,26 @@
+<html>
+  <head>
+    <style>
+      #peeps {
+        color:red;
+      }
+    </style>
+  </head>
+  <body>
+    <?php 
+	  function doSomething() {
+		  echo "Hello World";
+		  echo "<br>";
+		  echo "<img src ='' width='500px' height='500px'>";
+	  }
+	  doSomething();
+	  doSomething();
+	  doSomething();
+	  doSomething();
+	  doSomething();
+	  doSomething();
+	  doSomething();
+	  doSomething();
+    ?>
+  </body>
+</html>
